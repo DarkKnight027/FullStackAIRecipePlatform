a9ea910eb9180cb8ee7a0ea1584b7dcc73e6b2b6
DarkKnight027 <DarkKnight027@users.noreply.github.com>
2026-01-30 21:20:02 +0530

    fix: render actual logo using next/image and next/link

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -3,13 +3,18 @@
 import { SignedOut, SignedIn, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 import React from 'react'
 import { Button } from './ui/button';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Header = () => {
+    const user = null; //replace with actual user logic
   return (
     <header className='fixed top-0 w-full border-b border-stone-200 bg-stone-50/80 backdrop-blur-md z-50 supports-backdrop-blur-md
     '>  
     <nav className='containter mx-auto px-4 h-16 flex items-center justify-between    '>
-        Logo
+        <Link href={user ? "/dashboard" : "/"}>
+          <Image src="/logo2.png" alt="Logo" width={60} height={60} className="w-16" />
+        </Link>
 
         <div>
             Nav Links

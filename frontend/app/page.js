21c59d147baf253b1abdd70850c2ab96c931ca9f
DarkKnight027 <DarkKnight027@users.noreply.github.com>
2026-01-30 16:37:51 +0530

    Initial commit

---
+++ frontend/app/page.js
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant="primary" size='xl'>hELLO</Button></div>
+  );
+}
